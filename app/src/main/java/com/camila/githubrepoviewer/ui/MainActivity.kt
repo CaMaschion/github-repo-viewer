1616d0ab74074db04463a9718f9e4d89eb0dda78
Camila Maschion <cm.maschion@gmail.com>
2023-10-15 18:55:34 +0100

    repository di

--- app/src/main/java/com/camila/githubrepoviewer/ui/MainActivity.kt
+++ app/src/main/java/com/camila/githubrepoviewer/ui/MainActivity.kt
@@ -3,10 +3,15 @@ package com.camila.githubrepoviewer.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.camila.githubrepoviewer.R
+import com.camila.githubrepoviewer.repository.GithubRepository
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
